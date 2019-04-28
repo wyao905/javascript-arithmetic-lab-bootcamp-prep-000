3e99df7b0dfec42109a5aa8de0a2d79e23733f23
Warren Yao <warrenyca905@gmail.com>
2019-04-28 21:11:53 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,3 +1,12 @@
 function add(a, b) {
   return a + b;
 }
+ function subtract(a, b) {
+   return a - b;
+ }
+ function multiple(a, b) {
+   return a * b;
+ }
+ function divide(a, b) {
+   return a / b;
+ }
